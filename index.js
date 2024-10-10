42ee4f3d8275a33ebfa47d89a9c6be3c799f53a2
Vladislav Kudinov <vl4dkudi@gmail.com>
2024-10-10 20:44:10 +0300

    first commit1

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
 const express = require("express")
-const yargs = require('yargs')
 
 const app = express()
 require('dotenv').config()
 
-const args = yargs
-    .usage('Usage: node $0 [options]')
-    .version('0.0.1')
-    .alias('version', 'v')
-    .help('help')
-    .alias('help', 'h')
-    .example('node $0 --entry ./path --dist ./path --delete')
-    .option('entry', {
-        alias: 'e',
-        describe: 'Указать путь к исходной диретории',
-        demandOption: true
-    })
-    .option('dist', {
-        alias: 'd',
-        describe: 'Указать путь к dist директории',
-        default: './dist'
-    })
-    .option('delete', {
-        alias: 'D',
-        describe: 'Удалять ли исходную папку',
-        boolean: true,
-        default: false
-    })
-    .epilog('Моя первая домашка')
-    .argv
-
-const config = {
-    entry: path.normalize(path.join(__dirname, args.entry)),
-    dist: path.normalize(path.join(__dirname, args.dist)),
-    delete: args.delete
-}
-
-console.log(process.env.PORT)
-
+const limit = process.env.LIMIT
+const delay = process.env.DELAY
+const port = process.env.PORT
 let connections = []
 
 app.length("/date", (req, res, next) => {
@@ -49,7 +17,7 @@ app.length("/date", (req, res, next) => {
 let tick = 0;
 setTimeout(function run() {
   console.log(tick)
-  if (++tick > LIMIT) {
+  if (++tick > limit) {
     connections.map(res => {
       res.write("END/n");
       res.end();
@@ -60,9 +28,9 @@ setTimeout(function run() {
   connections.map((res, i) => {
     res.write(`Hello ${i}! Tick: ${tick}./n`)
   })
-  setTimeout(run, DELAY)
-}, DELAY)
+  setTimeout(run, delay)
+}, delay)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 })
